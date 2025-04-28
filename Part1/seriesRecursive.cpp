77c5da2ad9255f9caa84688aa7aa7b2cdf6f727c
xy-zenith <148595961+xy-zenith@users.noreply.github.com>
2025-04-27 17:23:46 -0700

    modify part 1

--- Part1/seriesRecursive.cpp
+++ Part1/seriesRecursive.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 #include <cstdlib> 
 
-//TODO: add your documentation here
-// TO DO: implement the recursive series function
-//      Hint: it should return a long long (integers)
-long seriesRecursive(long n){
-    if (n<=0) return -1;
-  if (n == 0) return 0;
-  if (n == 1 ||n==2) return 1;
-  return  (seriesRecursive(n-1) + 2*seriesRecursive(n-2)+ 4*seriesRecursive(n-3));
-  }
+/** recursively compute the n-th number of the sequence
+ *  J(n) = 0                                      if n = 0
+ *         1                                      if n = 1
+ *         1                                      if n = 2
+ *         J(n - 1) + 2 * J(n - 2) + 4 * J(n - 3) if n > 2
+ * @param n - index
+ * @return the n-th number of the sequence, 
+ *         or -1 if invalid (negative index)
+ */
+long long seriesRecursive(int n)
+{
+    if (n < 0) //case for n < 0
+        return -1;
 
-int main(int argc, char* argv[]) {
+    if (n == 0) //base case for n = 0
+        return 0;
+
+    if (n == 1 || n == 2) //base case for n = 1 and n = 2
+        return 1;
+
+    return (seriesRecursive(n - 1) + 2 * seriesRecursive(n - 2) + 4 * seriesRecursive(n - 3));
+}
+
+
+int main(int argc, char* argv[])
+{
     // Read n from command line argument
-    if (argc != 2) {
+    if (argc != 2)
+    {
         std::cerr << "Usage: " << argv[0] << " <n>" << std::endl;
         return 1; 
     }
